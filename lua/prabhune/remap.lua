cce00f56f8a58ed3026bc92f6bb652349d488025
ojasprabhune <prabhuneojas@gmail.com>
2025-07-23 23:17:13 -0400

    Finalizing backup

--- lua/prabhune/remap.lua
+++ lua/prabhune/remap.lua
@@ -7,9 +7,7 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Search and replace", silent = true })
 
 -- general commands
-vim.keymap.set("n", "<leader>ee", vim.cmd.NvimTreeOpen, { desc = "File explorer" })
-vim.keymap.set('n', '<leader>eq', vim.cmd.NvimTreeClose, { desc = "Close file explorer" })
-vim.keymap.set('n', '<leader>bb', vim.cmd.NvimTreeFindFile, { desc = "Focus workspace" })
+vim.keymap.set("n", "<leader>ee", vim.cmd.NvimTreeToggle, { desc = "File explorer" })
 
 vim.keymap.set("n", "<leader>l", vim.cmd.Lazy, { desc = "Lazy" })
 vim.keymap.set("n", "<leader>m", vim.cmd.Mason, { desc = "Mason" })
